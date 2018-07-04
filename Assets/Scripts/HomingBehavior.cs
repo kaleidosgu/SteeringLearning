48267467126c075324674de096f8bcacaa4277c4
kaleidosgu <gjcaoyi@163.com>
2018-07-04 09:52:53 +0800

    回巢火箭优化

--- Assets/Scripts/HomingBehavior.cs
+++ Assets/Scripts/HomingBehavior.cs
@@ -9,10 +9,20 @@ public class HomingBehavior : MonoBehaviour {
     public float SpeedOfRocket;
     public float TimeForIgnoringTarget;
     public Vector2 RandomRange;
+    public bool StartToIgnore;
+
+    public float AngleOfTurn;
+
+    public float RocketFixedTime;
 
     private Rigidbody2D m_rigidbody;
     private bool m_bActiveIgnore;
     private float m_fTimeForIgnoringTarget;
+
+    private float m_flastAngleOfTurn;
+    private bool m_bFixedTiming;
+    private float m_fCurFixedTime;
+    private int m_nCurrentIndex;
     // Use this for initialization
     void Start () {
         m_rigidbody = GetComponent<Rigidbody2D>();
@@ -22,24 +32,40 @@ public class HomingBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if(m_bActiveIgnore == true)
+        if(m_bActiveIgnore == true && StartToIgnore == true)
         {
             m_fTimeForIgnoringTarget += Time.deltaTime;
             if(m_fTimeForIgnoringTarget >= TimeForIgnoringTarget)
             {
+                m_bFixedTiming = true;
                 m_bActiveIgnore = false;
+                m_fCurFixedTime = 0.0f;
             }
+            m_rigidbody.velocity = transform.right * SpeedOfRocket;
         }
         else
         {
-            Vector2 point2Target = (Vector2)transform.position - (Vector2)TargetTransform.transform.position;
+            if (m_bFixedTiming == true)
+            {
+                if (m_fCurFixedTime >= RocketFixedTime)
+                {
+                    m_bFixedTiming = false;
+                }
+                m_fCurFixedTime += Time.deltaTime;
+                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + m_flastAngleOfTurn);
+                m_rigidbody.velocity = transform.right * SpeedOfRocket;
+            }
+            else
+            {
+                Vector2 point2Target = (Vector2)transform.position - (Vector2)TargetTransform.transform.position;
 
-            point2Target.Normalize();
+                point2Target.Normalize();
 
-            float value = Vector3.Cross(point2Target, transform.right).z;
-            float fChangeValueCache = rotatingSpeed * value;
-            m_rigidbody.angularVelocity = fChangeValueCache * Time.deltaTime;
-            m_rigidbody.velocity = transform.right * SpeedOfRocket;
+                float value = Vector3.Cross(point2Target, transform.right).z;
+                float fChangeValueCache = rotatingSpeed * value;
+                m_rigidbody.angularVelocity = fChangeValueCache * Time.deltaTime;
+                m_rigidbody.velocity = transform.right * SpeedOfRocket;
+            }
         }
     }
 
@@ -47,9 +73,21 @@ public class HomingBehavior : MonoBehaviour {
     {
         if( collision.gameObject.tag == "PlannetColliderTag")
         {
+            //m_bFixedTiming = true;
             m_bActiveIgnore = true;
-            m_rigidbody.angularVelocity = Random.Range(RandomRange.x, RandomRange.y);
+            //m_rigidbody.angularVelocity = Random.Range(RandomRange.x, RandomRange.y);
             m_fTimeForIgnoringTarget = 0.0f;
+            
+            if (m_nCurrentIndex == 0)
+            {
+                m_flastAngleOfTurn = AngleOfTurn;
+                m_nCurrentIndex = 1;
+            }
+            else
+            {
+                m_flastAngleOfTurn = -AngleOfTurn;
+                m_nCurrentIndex = 0;
+            }
         }
     }
 }
---
+++ Assets/Scripts/RocketDirectionWithAngle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketDirectionWithAngle : MonoBehaviour {
+
+    public float AngleToMove;
+    public float SpeedOfRocket;
+
+    private Rigidbody2D m_rigid2d;
+	// Use this for initialization
+	void Start () {
+        m_rigid2d = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + AngleToMove);
+        m_rigid2d.velocity = transform.right * SpeedOfRocket;
+
+    }
+}
