b5fe2951243faacef78ac89fe03aae7154eabd27
kaleidosgu <gjcaoyi@163.com>
2018-07-10 23:04:45 +0800

    三段判断避免碰撞添加

--- Assets/Scripts/RocketCollisionAvoidance.cs
+++ Assets/Scripts/RocketCollisionAvoidance.cs
@@ -22,25 +22,85 @@ public class RocketCollisionAvoidance : MonoBehaviour {
 		//有三条线。上中下，选择半径最近的值，对它进行回避。
 		m_rigid2d.velocity = transform.right * SpeedOfRocket;
 
-		bool bCollision = _isColliderCollision();
+		float fTransDistance = 0;
+		float fUpDistance = 0;
+		float fDownDistance = 0;
+		bool bCollision = _isColliderCollision(transform,out fTransDistance);
+		bool bCollisionUp = _isColliderCollision(TransUp,out fUpDistance);
+		bool bCollisionDown = _isColliderCollision(TransDown,out fDownDistance);
 		
-		if( bCollision == true )
+		bool bTrans = false;
+		bool bTransUp = false;
+		bool bTransDown = false;
+		if( fTransDistance < fUpDistance )
 		{
-			Vector3 vecEndPos = transform.right * DistanceOfCheck + TransDown.position;
-			Vector3 vecPos3 = vecEndPos - ColliderTarget.transform.position;
-			Vector2 vec2 = new Vector2( vecPos3.x,vecPos3.y );
-			Vector2 vecNom = vec2.normalized;
-			float fAng = Mathf.Atan2(vecNom.y,vecNom.x) * Mathf.Rad2Deg;
-			fAng = ForceOfAvoidance * fAng;
-			transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z + fAng);
+			if( fTransDistance < fDownDistance )
+			{
+				bTrans = true;
+			}
+			else
+			{
+				bTransDown = true;
+			}
 		}
+		else if( fUpDistance <= fTransDistance )
+		{
+			if( fUpDistance < fDownDistance )
+			{
+				bTransUp = true;
+			}
+			else
+			{
+				bTransDown = true;
+			}
+		}
+		if( bTrans == true && bCollision == true )
+		{
+			_rotateRocket(transform);
+		}
+		if( bTransUp == true && bCollisionUp == true )
+		{
+			_rotateRocket(transform);
+		}
+		if( bTransDown == true && bCollisionDown == true )
+		{
+			_rotateRocket(transform);
+		}
+		// if( bCollision == true )
+		// {
+		// 	Vector3 vecEndPos = transform.right * DistanceOfCheck + TransDown.position;
+		// 	Vector3 vecPos3 = vecEndPos - ColliderTarget.transform.position;
+		// 	Vector2 vec2 = new Vector2( vecPos3.x,vecPos3.y );
+		// 	Vector2 vecNom = vec2.normalized;
+		// 	float fAng = Mathf.Atan2(vecNom.y,vecNom.x) * Mathf.Rad2Deg;
+		// 	fAng = ForceOfAvoidance * fAng;
+		// 	transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z + fAng);
+		// }
+	}
+
+	private void _rotateRocket(Transform transRotate)
+	{
+		Vector3 vecEndPos = transform.right * DistanceOfCheck + transRotate.position;
+		Vector3 vecPos3 = vecEndPos - ColliderTarget.transform.position;
+		Vector2 vec2 = new Vector2( vecPos3.x,vecPos3.y );
+		Vector2 vecNom = vec2.normalized;
+		float fAng = Mathf.Atan2(vecNom.y,vecNom.x) * Mathf.Rad2Deg;
+		fAng = ForceOfAvoidance * fAng;
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z + fAng);
 	}
 
 	void OnDrawGizmos()
 	{
-		Vector3 vecEndPos = _getTransformEndPos(transform);
+		_GizemosTransformProcess(transform);
+		_GizemosTransformProcess(TransDown);
+		_GizemosTransformProcess(TransUp);
+	}
 
-		if( _isColliderCollision() )
+	private void _GizemosTransformProcess(Transform transGizmos)
+	{
+		Vector3 vecEndPos = _getTransformEndPos(transGizmos);
+		float fDistance = 0;
+		if( _isColliderCollision(transGizmos,out fDistance) )
 		{
 			Gizmos.color = Color.red;
 		}
@@ -48,16 +108,7 @@ public class RocketCollisionAvoidance : MonoBehaviour {
 		{
 			Gizmos.color = Color.blue;	
 		}
-
-		Gizmos.DrawLine(transform.position,vecEndPos) ;
-
-		Vector3 vecEndPos1 = _getTransformEndPos(TransDown);
-
-		Gizmos.DrawLine(TransDown.position,vecEndPos1) ;
-		
-		Vector3 vecEndPos2 = _getTransformEndPos(TransUp);
-
-		Gizmos.DrawLine(TransUp.position,vecEndPos2) ;
+		Gizmos.DrawLine(transGizmos.position,vecEndPos) ;
 	}
 
 	private Vector3 _getTransformEndPos(Transform trans)
@@ -66,11 +117,11 @@ public class RocketCollisionAvoidance : MonoBehaviour {
 		return vecEndPos;
 	}
 
-	private bool _isColliderCollision()
+	private bool _isColliderCollision(Transform transCheck, out float outDistance)
 	{
 		bool bRes = false;
 		Vector3 vecCollider = ColliderTarget.transform.position;
-		Vector3 vecEndPos = transform.right * DistanceOfCheck + TransDown.position;
+		Vector3 vecEndPos = transform.right * DistanceOfCheck + transCheck.position;
 		float fDistance = Mathf.Sqrt((vecCollider.x - vecEndPos.x) * (vecCollider.x - vecEndPos.x) + (vecCollider.y - vecEndPos.y) * (vecCollider.y - vecEndPos.y));
 		if( fDistance < ColliderTarget.radius )
 		{
@@ -80,6 +131,7 @@ public class RocketCollisionAvoidance : MonoBehaviour {
 		{
 			bRes = false;
 		}
+		outDistance = fDistance;
 		return bRes;
 	}
 }
