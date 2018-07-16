2bc5bc519a84382085196b2cea9ce96c6aae0910
kaleidosgu <gjcaoyi@163.com>
2018-07-16 18:53:19 +0800

    添加对象

--- Assets/Scripts/RocketAnimationCurve.cs
+++ Assets/Scripts/RocketAnimationCurve.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class RocketAnimationCurve : MonoBehaviour {
 
+    public Transform transTarget;
     public AnimationCurve AnimCurve;
     public float TimesToBiggerX;
     public float TimesToBiggerY;
-
-    private bool m_bUpdateOnce;
+    
     private Vector3 m_vecLastLocalPos;
     private Vector3 m_vecLastAdd;
     // Use this for initialization
@@ -23,15 +23,6 @@ public class RocketAnimationCurve : MonoBehaviour {
 	void Update () {
 		
         Vector3 vec = new Vector3(Time.time * TimesToBiggerX, AnimCurve.Evaluate(Time.time) * TimesToBiggerY, 0);
-        //if(m_bUpdateOnce == false)
-        //{
-        //    transform.localPosition = vec + transform.localPosition;
-        //    m_bUpdateOnce = true;
-        //}
-        //else
-        //{
-        //    transform.localPosition = vec ;
-        //}
         transform.localPosition = m_vecLastLocalPos + vec - m_vecLastAdd;
         m_vecLastAdd = vec;
         m_vecLastLocalPos = transform.localPosition;
