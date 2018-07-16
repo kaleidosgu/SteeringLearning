74789d40a05f47963efaf000c632d5bd8c7f6249
kaleidosgu <gjcaoyi@163.com>
2018-07-16 16:49:43 +0800

    添加一个AnimationCurve驱动的移动

---
+++ Assets/Scripts/RocketAnimationCurve.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAnimationCurve : MonoBehaviour {
+
+    public AnimationCurve AnimCurve;
+    public float TimesToBiggerX;
+    public float TimesToBiggerY;
+    // Use this for initialization
+    void Start () {
+        AnimCurve.postWrapMode = WrapMode.Loop;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        Vector3 vec = new Vector3(Time.time * TimesToBiggerX, AnimCurve.Evaluate(Time.time) * TimesToBiggerY, 0);
+        transform.position = vec;
+
+    }
+}
