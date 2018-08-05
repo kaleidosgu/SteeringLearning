878e2b5b6c2d20ad9db0e2432ae283742337c60a
kaleidosgu <gjcaoyi@163.com>
2018-08-05 10:29:25 +0800

    旋转animationCurve

--- Assets/Scripts/RocketAnimationCurveRotate.cs
+++ Assets/Scripts/RocketAnimationCurveRotate.cs
@@ -22,9 +22,15 @@ public class RocketAnimationCurveRotate : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        Vector3 vec = new Vector3(Time.time * TimesToBiggerX, AnimCurve.Evaluate(Time.time) * TimesToBiggerY, 0);
-        transform.localPosition = m_vecLastLocalPos + vec - m_vecLastAdd;
-        m_vecLastAdd = vec;
+        Vector3 _vecAniCurve = new Vector3(Time.time * TimesToBiggerX, AnimCurve.Evaluate(Time.time) * TimesToBiggerY, 0);
+
+        Vector3 vecNewPos = m_vecLastLocalPos + _vecAniCurve - m_vecLastAdd;
+        Vector3 diff = vecNewPos - transform.localPosition;
+        float angle = Mathf.Atan2(diff.y, diff.x);
+        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg*angle);
+
+        transform.localPosition = vecNewPos;
+        m_vecLastAdd = _vecAniCurve;
         m_vecLastLocalPos = transform.localPosition;
     }
 }
