699dacc51ad0ec794affe0dc20dcc97805747cdf
kaleidosgu <gjcaoyi@163.com>
2018-08-05 10:02:12 +0800

    添加AnimationCurve的rotate版本

--- Assets/Scripts/RocketAnimationCurveRotate.cs
+++ Assets/Scripts/RocketAnimationCurveRotate.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RocketAnimationCurve : MonoBehaviour {
+public class RocketAnimationCurveRotate : MonoBehaviour {
 
     public Transform transTarget;
     public AnimationCurve AnimCurve;
