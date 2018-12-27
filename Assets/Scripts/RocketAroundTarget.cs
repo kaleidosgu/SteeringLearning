bb85c681b64c61f3ab06d061689caaa98c2e65a2
kaleidos <gjcaoyi@163.com>
2018-12-27 22:33:45 +0800

    目标组移动添加

---
+++ Assets/Scripts/RocketAroundTarget.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAroundTarget : MonoBehaviour
+{
+
+    public List<Transform> LstTargets;
+    private int m_nCurrentIndex;
+    private int m_nCountsTarget;
+    private HomingBehavior m_homingBehavior;
+    private Collider2D m_lastCollider;
+    // Use this for initialization
+    void Start()
+    {
+        m_nCountsTarget = LstTargets.Count;
+        m_homingBehavior = GetComponent<HomingBehavior>();
+        if (m_nCurrentIndex >= 0 && m_nCurrentIndex < m_nCountsTarget)
+        {
+            m_homingBehavior.TargetTransform = LstTargets[m_nCurrentIndex];
+            m_lastCollider = LstTargets[m_nCurrentIndex].GetComponent<Collider2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlannetColliderTag")
+        {
+            if(m_lastCollider != null)
+            {
+                m_lastCollider = collision;
+                if( m_nCurrentIndex < m_nCountsTarget - 1)
+                {
+                    m_nCurrentIndex++;
+                }
+                else
+                {
+                    m_nCurrentIndex = 0;
+                }
+                m_homingBehavior.TargetTransform = LstTargets[m_nCurrentIndex];
+                Debug.Log(string.Format("m_nCurrentIndex {0}", m_nCurrentIndex));
+            }
+        }
+    }
+}
