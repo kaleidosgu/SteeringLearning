580c536de12ccc438c97b586dc673c7519064e5d
kaleidos <gjcaoyi@163.com>
2019-01-06 22:58:52 +0800

    虽然有点乱，但是偏角做好了

--- Assets/Scripts/RocketChangeRotationSpeed.cs
+++ Assets/Scripts/RocketChangeRotationSpeed.cs
@@ -6,9 +6,10 @@ public class RocketChangeRotationSpeed : MonoBehaviour {
 
     public float ChangeRotatingSpeed;
     public float TimeToChangeRotating;
+    public Transform TargetCenterTrans;
+    public float turnSpeed;
 
     private float m_fCurrentTimeToChange;
-    private Collider2D m_lastCollider;
     private HomingBehavior m_homingBehavior;
 
     private bool m_bChangeRotating;
@@ -27,13 +28,25 @@ public class RocketChangeRotationSpeed : MonoBehaviour {
             {
                 m_bChangeRotating = false;
                 m_homingBehavior.ResetRotatingSpeed();
+                m_homingBehavior.enabled = true;
             }
+
+            Vector2 vecSelf = new Vector2(transform.position.x, transform.position.y);
+            Vector2 vecTarget = new Vector2(TargetCenterTrans.position.x, TargetCenterTrans.position.y);
+            Vector2 direction = vecTarget - vecSelf;
+            float toRotation = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+
+            float rotation = Mathf.LerpAngle(transform.rotation.eulerAngles.z, toRotation, Time.deltaTime * turnSpeed);
+
+            transform.rotation = Quaternion.Euler(0, 0, rotation);
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        m_homingBehavior.ChangeRotationSpeed(ChangeRotatingSpeed);
+        m_homingBehavior.enabled = false;
         m_bChangeRotating = true;
         m_fCurrentTimeToChange = 0.0f;
+
+
     }
 }
